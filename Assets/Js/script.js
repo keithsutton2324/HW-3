837886300860135b1bb66c144691728230754bde
ksutton <suttonk@sbcglobal.net>
2020-10-15 15:58:44 -0700

    Completed final logic and data for password generator

---
+++ Assets/Js/script.js
@@ -0,0 +1,77 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Prompt user for password criteria
+function generatePassword() {
+  var passwordLength = prompt("Password length must be at least 8 and no greater than 128. Please enter password length:", "");
+
+  passwordLength = parseInt(passwordLength)
+  if (Number.isNaN(passwordLength)) {
+    console.log(passwordLength)
+    console.log("Non-numeric input or user cancel: " + passwordLength)
+    return
+  }
+  else if (passwordLength && passwordLength >= 8 && passwordLength <= 128) {
+    console.log("length in range: " + passwordLength)
+  }
+  else {
+    console.log(passwordLength)
+    console.log("length out of range: " + passwordLength)
+    // prompt again
+    generatePassword()
+  }
+
+  var validChars = ""
+
+  if (passwordLength) {
+    var useLowercase = confirm("Do you want to include lowercase characters?");
+    if (useLowercase) {
+      validChars += "abcdefghijklmnopqrstuvwxyz"
+    }
+
+    var useUppercase = confirm("Do you want to include uppercase characters?");
+    if (useUppercase) {
+      validChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+
+    var useNumeric = confirm("Do you want to include numeric characters?");
+    if (useNumeric) {
+      validChars += "0123456789"
+    }
+
+    var useSpecial = confirm("Do you want to include special characters?");
+    if (useSpecial) {
+      //validChars += "~!@#$%^&*()_+{}|:"`-=;',./"    // JS doesn't like "
+      validChars += "~!@#$%^&*()_+{}|:?`-=;',./"
+    }
+  }
+
+  console.log("passwordLength: ", passwordLength, "validChars: ", validChars)
+  var password = ""
+
+  if (useLowercase || useUppercase || useNumeric || useSpecial) {
+    for (var i = 0; i < passwordLength; i++) {
+      // Generate a random number between 1 and passwordLength
+      // Math.floor will round down, meaning we would get a number between
+      //0 and passwordLength, so we'll always add 1 to bump it up.
+      var idx = Math.floor(Math.random() * validChars.length) + 1;
+      password += validChars[idx]
+    }
+  }
+
+  // Display password
+  console.log("password: ", password);
+  document.getElementById("password1").innerHTML = password
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# 03 JavaScript: Password Generator
+
+
+```
+Utility that generates a strong random password based on user selection of password criteria.
+```
+
+```
+Generate a new, secure password:
+Click the button to generate a password
+You will be presented with a series of prompts for password criteria to be included in the password
+Choose a length of at least 8 characters and no more than 128 characters
+Choose lowercase, uppercase, numeric, and/or special characters to be include in the new password
+Input will be validated such that at least one character type is selected
+When all prompts are answered a password that matches the selected criteria is generated and displayed on the webpage 
+```
+
+Link to deployed application:
+
+
